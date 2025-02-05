c012f2ae74d29187d4c4dd1171b364871495217b
Moh. Rofiqi <88334099+ficilabs@users.noreply.github.com>
2025-02-05 23:57:30 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,46 +1,28 @@
 const express = require('express');
-const { createServer } = require('http');
+const http = require('http');
 const { Server } = require('socket.io');
-const mqtt = require('mqtt');
+const path = require('path');
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*"
-  }
-});
+const server = http.createServer(app);
+const io = new Server(server);
 
-// MQTT Configuration
-const broker = 'wss://broker.hivemq.com:1883/mqtt';
-const clientId = 'server_' + Math.random().toString(16).substr(2, 8);
-const topicPrefix = 'ficilabs/';
-
-const mqttClient = mqtt.connect(broker, {
-  clientId: clientId,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30000
-});
-
-mqttClient.on('connect', () => {
-  console.log('Connected to MQTT broker');
-  mqttClient.subscribe([`${topicPrefix}+input1`]);
-  mqttClient.subscribe([`${topicPrefix}+output1`]);
-});
-
-mqttClient.on('message', (topic, message) => {
-  const topicType = topic.split('/')[1];
-  io.emit(topicType, message.toString());
-});
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-
-  socket.on('toggleInput', (data) => {
-    const { lightId, state } = data;
-    mqttClient.publish(`${topicPrefix}input${lightId}`, state);
-  });
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
-module.exports = httpServer;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
